ada96b9a1578d6fc882e9a9d7094e685f0f8032f
OlaPapiGrr <bryan.amrein@gmx.ch>
2024-12-12 14:31:58 +0100

    linter fixed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import chalk from 'chalk'
 console.log(chalk.blue('It works'));
 
 let myUnusedVariable = 42; 
-if (myUnusedVariable == '42') { 
+if (myUnusedVariable === 42) { 
   console.log('ESLint Fehler erzeugt!');
 }

780fc62e0e1eeb30c3dd7625162cdb5d3c40d7ed
OlaPapiGrr <bryan.amrein@gmx.ch>
2024-12-12 14:31:12 +0100

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import globals from "globals";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+
+  {
+    rules:{
+      'eqeqeq': 'error',        // Erzwingt die Verwendung von === statt ==
+    }
+  }
+
+];
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 import chalk from 'chalk'
 console.log(chalk.blue('It works'));
+
+let myUnusedVariable = 42; 
+if (myUnusedVariable == '42') { 
+  console.log('ESLint Fehler erzeugt!');
+}
